1b2f10cbbbb99810582bd48899c9d61145ba2469
Taylor Osbourne <taylorosbourne@taylors-mbp.lan>
2019-09-05 21:10:19 -0400

    bumped up coffee cup

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,12 +2,12 @@ import React from "react"
 import styled from "styled-components"
 
 const Link = styled.a`
-    text-decoration: none;
-    color: #173449;
-    font-weight: bold;
-    font-family: Arial;
-    position: relative;
-    :before {
+  text-decoration: none;
+  color: #173449;
+  font-weight: bold;
+  font-family: Arial;
+  position: relative;
+  :before {
     content: "";
     position: absolute;
     width: 100%;
@@ -20,24 +20,37 @@ const Link = styled.a`
     transform: scaleX(0);
     -webkit-transition: all 0.3s ease-in-out 0s;
     transition: all 0.3s ease-in-out 0s;
-    }
-    :hover:before {
+  }
+  :hover:before {
     visibility: visible;
     -webkit-transform: scaleX(1);
     transform: scaleX(1);
-}
+  }
 `
 
-
 const Footer = () => (
-    <footer style={{
-        display: `flex`,
-        justifyContent: `center`
-    }}>
-        <p style={{ maxWidth: 500 }}>
-            Made with <span aria-label="love" role="img">❤️</span>, <span aria-label="coffee" role="img">☕</span>, and  <Link href="https://www.gatsbyjs.org/" target="_blank" alt="gatsby-js">Gatsby</Link>.
-        </p>
-    </footer>
+  <footer
+    style={{
+      display: `flex`,
+      justifyContent: `center`,
+    }}
+  >
+    <p style={{ maxWidth: 500 }}>
+      Made with{" "}
+      <span aria-label="love" role="img">
+        ❤️
+      </span>
+      ,{" "}
+      <span aria-label="coffee" role="img" style={{ fontSize: `1.3rem` }}>
+        ☕
+      </span>
+      , and{" "}
+      <Link href="https://www.gatsbyjs.org/" target="_blank" alt="gatsby-js">
+        Gatsby
+      </Link>
+      .
+    </p>
+  </footer>
 )
 
 export default Footer
