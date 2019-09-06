d08d2fbc438659be1b2b4d9746b9f43c6a407ed0
Taylor Osbourne <taylorosbourne@taylors-mbp.lan>
2019-09-05 21:03:26 -0400

    decreased layout maxwidth

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,8 +11,8 @@ import PropTypes from "prop-types"
 
 // import Header from "./header"
 import "./layout.css"
-import Nav from "./nav";
-import Footer from "./footer";
+import Nav from "./nav"
+import Footer from "./footer"
 
 const Layout = ({ children }) => {
   // const data = useStaticQuery(graphql`
@@ -28,17 +28,17 @@ const Layout = ({ children }) => {
   return (
     <>
       {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
-      <Nav/>
+      <Nav />
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
+          maxWidth: 800,
           padding: `0px 1.0875rem 1.45rem`,
           paddingTop: 0,
         }}
       >
         <main>{children}</main>
-        <Footer/>
+        <Footer />
       </div>
     </>
   )
