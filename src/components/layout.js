00a130b27808054ed7f3dcc0f824f5276f8643d1
Taylor Osbourne <taylorosbourne@taylors-mbp.lan>
2019-09-05 21:06:08 -0400

    removed unneccessary comments

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,6 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 // import { useStaticQuery, graphql } from "gatsby"
-
-// import Header from "./header"
 import "./layout.css"
 import Nav from "./nav"
 import Footer from "./footer"
@@ -27,7 +18,6 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
       <Nav />
       <div
         style={{
