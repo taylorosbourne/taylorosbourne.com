d93bf1da27e9928e945a30ce5d1353598022575a
Taylor Osbourne <taylorosbourne@taylors-mbp.lan>
2019-11-03 21:43:01 -0500

    nav is responsive

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
+
+const StyledNav = styled.nav`
+    padding-top: 25px;
+    max-width: 90%;
+    height: 80px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0 auto;
+`
 
 const Nav = () => (
-    <nav style={{
-        paddingTop: `25px`,
-        maxWidth: `1200px`,
-        height: `80px`,
-        display: `flex`,
-        justifyContent: `space-between`,
-        alignItems: `center`,
-        margin: `0 auto`
-    }}>
+    <StyledNav>
         <h5 style={{
             paddingTop: `25px`
         }}><Link style={{textDecoration: `none`, color: `#173449`}} to="/">console.blog( <span aria-label="fire" role="img">🔥</span>)</Link>
@@ -41,7 +44,7 @@ const Nav = () => (
                 <svg xmlns="http://www.w3.org/2000/svg" fill="#173449" width="25" height="25" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-2 16h-2v-6h2v6zm-1-6.891c-.607 0-1.1-.496-1.1-1.109 0-.612.492-1.109 1.1-1.109s1.1.497 1.1 1.109c0 .613-.493 1.109-1.1 1.109zm8 6.891h-1.998v-2.861c0-1.881-2.002-1.722-2.002 0v2.861h-2v-6h2v1.093c.872-1.616 4-1.736 4 1.548v3.359z" /></svg>
             </a>
         </div>
-    </nav>
+    </StyledNav>
 );
 
 export default Nav;
