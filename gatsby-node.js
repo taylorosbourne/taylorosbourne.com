8ea81e3f5a73e268d4247203cbc64bb620f91b8c
Taylor Osbourne <taylorosbourne@taylors-mbp.lan>
2019-11-04 17:26:17 -0500

    still messing up the create page for tagged posts, look into this later

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,14 @@
 const path = require('path')
+// const _ = require('lodash')
 
 exports.createPages = ({actions, graphql}) => {
     const {createPage} = actions
+
+    const templates = {
+      posts: path.resolve('src/templates/blogPost.js'),
+      // tagPosts: path.resolve('src/templates/tagPosts.js')
+    }
+
     const postTemplate = path.resolve('src/templates/blogPost.js')
 
     return graphql(`
@@ -26,48 +33,32 @@ exports.createPages = ({actions, graphql}) => {
             return Promise.reject(res.errors)
         }
 
-        res.data.allMarkdownRemark.edges.forEach(({node}) => {
+        const posts = res.data.allMarkdownRemark.edges
+        posts.forEach(({node}) => {
             createPage({
                 path: node.frontmatter.path,
                 component: postTemplate
             })
         })
+
+        // let tags = []
+        // _.each(posts, edge => {
+        //   if (_.get(edge, 'node.frontmatter.tags')) {
+        //     tags = tags.concat(edge.node.frontmatter.tags)
+        //   }
+        // })
+
+        // tags = _.uniq(tags)
+
+        // tags.forEach(tag => (
+        //   createPage({
+        //     path: `/tag/${tag}`,
+        //     component: templates.tagPosts,
+        //     context: {
+        //       tag
+        //     }
+        //   })
+        // ))
+
     })
 }
-
-// exports.createPages = ({actions, graphql}) => {
-//   const {createPage} = actions
-//   const tagPostsTemplate = path.resolve('src/templates/tagPosts.js')
-
-//   return graphql(`
-//       {
-//           allMarkdownRemark {
-//               edges {
-//                 node {
-//                   html
-//                   id
-//                   frontmatter {
-//                     path
-//                     title
-//                     date
-//                     author
-//                     tag
-//                   }
-//                 }
-//               }
-//             }
-//       }
-//   `).then(res  => {
-//       if(res.errors) {
-//           return Promise.reject(res.errors)
-//       }
-
-//       res.data.allMarkdownRemark.edges.forEach(({node}) => {
-//           createPage({
-//               path: node.frontmatter.tag,
-//               tag: node.frontmatter.tag,
-//               component: tagPostsTemplate
-//           })
-//       })
-//   })
-// }
