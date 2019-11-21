c42b4cf0341449450258a455564403ce33c1d83e
Taylor Osbourne <taylorosbourne@taylors-mbp.lan>
2019-11-20 22:39:21 -0500

    limit blog posts to three most recent on index

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import Form from '../components/form'
 import PostList from '../components/postList';
 import GeneralInfo from '../components/generalInfo';
 import ExperienceSection from '../components/experience';
+import { Tag } from '../styles/Link';
 
 const IndexPage = ({ data }) => {
   const posts = data.allMarkdownRemark.edges;
@@ -17,7 +18,7 @@ const IndexPage = ({ data }) => {
       <br />
       <GeneralInfo />
       <ExperienceSection />
-      <h2 style={{marginTop: `85px`}}>console.blog(<span aria-label="fire" role="img">🔥</span>)</h2>
+      <h2 style={{marginTop: `85px`}}>console.blog(<span aria-label="fire" role="img">🔥</span>) <Tag to="/blog" style={{fontSize: `1rem`}}>View All</Tag></h2>
       <PostList posts={posts} />
       <h2 style={{marginTop: `35px`}}>Want to chat? <span aria-label="waving" role="img">👋</span></h2>
       <br/>
@@ -26,9 +27,9 @@ const IndexPage = ({ data }) => {
   );
 };
 
-export const ALL_POSTS_QUERY = graphql`
-  query ALL_POSTS_QUERY {
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+export const RECENT_POSTS_QUERY = graphql`
+  query RECENT_POSTS_QUERY {
+    allMarkdownRemark(limit: 3, sort: { fields: [frontmatter___date], order: DESC }) {
       edges {
         node {
           id
