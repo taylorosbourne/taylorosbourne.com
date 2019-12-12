2c330432741e57f5076a86db0f4c049a77b82b01
Taylor-Osbourne <Taylor.osbourne@coxautoinc.com>
2019-12-11 19:17:38 -0500

    cleaned up config

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,74 +13,6 @@ module.exports = {
     `gatsby-transformer-remark`,
     `gatsby-plugin-sharp`,
     {
-    resolve: `gatsby-transformer-remark`,
-    options: {
-      plugins: [
-        {
-          resolve: `gatsby-remark-prismjs`,
-          options: {
-            // Class prefix for <pre> tags containing syntax highlighting;
-            // defaults to 'language-' (e.g. <pre class="language-js">).
-            // If your site loads Prism into the browser at runtime,
-            // (e.g. for use with libraries like react-live),
-            // you may use this to prevent Prism from re-processing syntax.
-            // This is an uncommon use-case though;
-            // If you're unsure, it's best to use the default value.
-            classPrefix: "language-",
-            // This is used to allow setting a language for inline code
-            // (i.e. single backticks) by creating a separator.
-            // This separator is a string and will do no white-space
-            // stripping.
-            // A suggested value for English speakers is the non-ascii
-            // character '›'.
-            inlineCodeMarker: null,
-            // This lets you set up language aliases.  For example,
-            // setting this to '{ sh: "bash" }' will let you use
-            // the language "sh" which will highlight using the
-            // bash highlighter.
-            aliases: {},
-            // This toggles the display of line numbers globally alongside the code.
-            // To use it, add the following line in gatsby-browser.js
-            // right after importing the prism color scheme:
-            //  require("prismjs/plugins/line-numbers/prism-line-numbers.css")
-            // Defaults to false.
-            // If you wish to only show line numbers on certain code blocks,
-            // leave false and use the {numberLines: true} syntax below
-            showLineNumbers: false,
-            // If setting this to true, the parser won't handle and highlight inline
-            // code used in markdown i.e. single backtick code like `this`.
-            noInlineHighlight: false,
-            // This adds a new language definition to Prism or extend an already
-            // existing language definition. More details on this option can be
-            // found under the header "Add new language definition or extend an
-            // existing language" below.
-            languageExtensions: [
-              {
-                language: "superscript",
-                extend: "javascript",
-                definition: {
-                  superscript_types: /(SuperType)/,
-                },
-                insertBefore: {
-                  function: {
-                    superscript_keywords: /(superif|superelse)/,
-                  },
-                },
-              },
-            ],
-            // Customize the prompt used in shell output
-            // Values below are default
-            prompt: {
-              user: "root",
-              host: "localhost",
-              global: false,
-            },
-          },
-        },
-      ],
-    },
-  },
-    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
@@ -92,6 +24,38 @@ module.exports = {
       options: {
         name: `pages`,
         path: `${__dirname}/src/pages`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [{
+          resolve: `gatsby-remark-prismjs`,
+          options: {
+            classPrefix: "language-",
+            inlineCodeMarker: null,
+            aliases: {},
+            showLineNumbers: false,
+            noInlineHighlight: false,
+            languageExtensions: [{
+              language: "superscript",
+              extend: "javascript",
+              definition: {
+                superscript_types: /(SuperType)/,
+              },
+              insertBefore: {
+                function: {
+                  superscript_keywords: /(superif|superelse)/,
+                },
+              },
+            }, ],
+            prompt: {
+              user: "root",
+              host: "localhost",
+              global: false,
+            },
+          },
+        }, ],
       },
     },
     {
@@ -109,20 +73,26 @@ module.exports = {
             }
           }
         `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.edges.map(edge => {
-                return Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.excerpt,
-                  date: edge.node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  custom_elements: [{ "content:encoded": edge.node.html }],
-                })
+        feeds: [{
+          serialize: ({
+            query: {
+              site,
+              allMarkdownRemark
+            }
+          }) => {
+            return allMarkdownRemark.edges.map(edge => {
+              return Object.assign({}, edge.node.frontmatter, {
+                description: edge.node.excerpt,
+                date: edge.node.frontmatter.date,
+                url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                custom_elements: [{
+                  "content:encoded": edge.node.html
+                }],
               })
-            },
-            query: `
+            })
+          },
+          query: `
               {
                 allMarkdownRemark(
                   sort: { order: DESC, fields: [frontmatter___date] },
@@ -141,10 +111,9 @@ module.exports = {
                 }
               }
             `,
-            output: `/rss.xml`,
-            title: `Taylor Osbourne - RSS Feed`,
-          },
-        ],
+          output: `/rss.xml`,
+          title: `Taylor Osbourne - RSS Feed`,
+        }, ],
       },
     },
     {
