bd1f1a4ee109a20491624f1ac0543fddec91f1eb
Taylor Osbourne <taylorgosbourne@gmail.com>
2019-12-20 21:43:45 -0500

    sketchy resume layout...

--- src/components/resumeSheet.js
+++ src/components/resumeSheet.js
@@ -7,20 +7,20 @@ const Sheet = styled.section`
   border: 3px dashed #dc322f;
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
+  justify-content: flex-start;
   align-items: center;
 `;
 
 const Section = styled.section`
   width: 100%;
-  height: 20rem;
-  border: 1px solid #b58900;
+  height: 18rem;
+  /* border: 1px solid #b58900; */
   display: flex;
   justify-content: space-between;
   div {
     height: 100%;
     width: 48.5%;
-    border: 1px solid #dc322f;
+    /* border: 1px solid #dc322f; */
     padding: 22px;
     h2 {
       margin-top: 0;
@@ -48,7 +48,6 @@ const Section = styled.section`
         display: inline-block;
         margin: 5px 0;
         color: #2aa198;
-        
       }
     }
   }
@@ -86,9 +85,40 @@ const ResumeSheet = () => (
           <li>Linkedin:</li>
         </ul>
       </div>
-      <div></div>
+      <div>
+        <dl>
+          <dt>INSTRUCTOR ASSOCIATE - GENERAL ASSEMBLY</dt>
+          <span>JAN 2019 - MAY 2019</span>
+          <dd>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</dd>
+        </dl>
+      </div>
     </Section>
-    <Section></Section>
+    <Section>
+    <div>
+        <h2>Skills</h2>
+        <div style={{display: `flex`, width: `100%`, justifyContent: `space-between`, alignItems: `flex-start`}}>
+          <ul style={{margin: 0, padding: 0, width: `48.5%`}}>
+            <li>JavaScript</li>
+            <li>React</li>
+            <li>Node</li>
+            <li>Express</li>
+            <li>Graphql</li>
+          </ul>
+          <ul style={{margin: 0, padding: 0, width: `48.5%`}}>
+            <li>Ruby</li>
+            <li>MongoDB</li>
+            <li>git</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <dl>
+          <dt>SOFTWARE DEVELOPER - MANHEIM</dt>
+          <span>APR 2013 - </span>
+          <dd>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</dd>
+        </dl>
+      </div>
+    </Section>
   </Sheet>
 );
 
